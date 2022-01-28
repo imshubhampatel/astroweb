2973e325f7f49cf4d58b66c8b3b3fc1ea1e55b75
Anshul Raman <anshul.raman@live.com>
2022-01-29 01:48:10 +0530

    added services and faq section

---
+++ components/faqComponent/Faqs.js
@@ -0,0 +1,106 @@
+import styles from "../../styles/components/faqComponent/Faqs.module.css";
+import { FaqCard } from "./FaqCard";
+import { useState } from "react";
+
+export function Faqs() {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  return (
+    <>
+      <div className={styles.container}>
+        {/* Buttons */}
+        <div className={styles.button_container}>
+          <div
+            className={`${styles.button} ${
+              currentTab == 0 ? styles.active : ""
+            } `}
+            onClick={() => setCurrentTab(0)}
+          >
+            Talk To Astrologer
+          </div>
+
+          <div
+            className={`${styles.button} ${
+              currentTab == 1 ? styles.active : ""
+            } `}
+            onClick={() => setCurrentTab(1)}
+          >
+            Misc Details
+          </div>
+
+          <div
+            className={`${styles.button} ${
+              currentTab == 2 ? styles.active : ""
+            } `}
+            onClick={() => setCurrentTab(2)}
+          >
+            Others
+          </div>
+        </div>
+
+        {/* FAQ Container */}
+        <div className={styles.faq_container}>
+          {currentTab == 0 && (
+            <>
+              <FaqCard
+                question="Can I cancel the appointment?"
+                answer="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the"
+              />
+              <FaqCard
+                question="How can I download this app?"
+                answer="Lorem Ipsum is simply 
+            dummy text of the printing and 
+            typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the"
+              />
+              <FaqCard
+                question="Can I register on the app as a Astrologer?"
+                answer="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the"
+              />
+              <FaqCard
+                question="What is the medium of instruction and communication?"
+                answer="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the"
+              />
+            </>
+          )}
+
+          {currentTab == 1 && (
+            <>
+              <FaqCard
+                question="Question on Misc Details "
+                answer="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the"
+              />
+              <FaqCard
+                question="How can I download this app?"
+                answer="Lorem Ipsum is simply 
+            dummy text of the printing and 
+            typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the"
+              />
+              <FaqCard
+                question="Can I register on the app as a Astrologer?"
+                answer="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the"
+              />
+            </>
+          )}
+
+          {currentTab == 2 && (
+            <>
+              <FaqCard
+                question="Question on Other details"
+                answer="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the"
+              />
+              <FaqCard
+                question="How can I download this app?"
+                answer="Lorem Ipsum is simply 
+            dummy text of the printing and 
+            typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the"
+              />
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,16 @@ import styles from "../styles/pages/index.module.css";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-import HeroImage from "../public/images/launchingSoon.png";
+import HeroImage from "../public/images/mascot.png";
+import Logo from "../public/images/logo_transparent.png"
+
+import { Faqs } from "../components/faqComponent/Faqs";
+
+import { BsChatSquareDots, BsBarChart } from "react-icons/bs";
+import { FiPhone, FiShoppingCart } from "react-icons/fi";
+import { MdOndemandVideo } from "react-icons/md";
+import { HiOutlineDocumentText } from "react-icons/hi";
+import { BiPlanet } from "react-icons/bi";
 
 const calculateTimeLeft = () => {
   const launchDate = new Date("February 15, 2022 03:00:00");
@@ -40,15 +49,19 @@ export default function Home() {
     }
 
     timerComponents.push(
-      <div key={interval}  >
-        <div className={`${styles.timerNumber}
+      <div key={interval}>
+        <div
+          className={`${styles.timerNumber}
 
-            ${ interval == "days" ? styles.days : "" }
-            ${ interval == "hours" ? styles.hours : "" }
-            ${ interval == "minutes" ? styles.minutes : "" }
-            ${ interval == "seconds" ? styles.seconds : "" }
+            ${interval == "days" ? styles.days : ""}
+            ${interval == "hours" ? styles.hours : ""}
+            ${interval == "minutes" ? styles.minutes : ""}
+            ${interval == "seconds" ? styles.seconds : ""}
 
-        `} >{timeLeft[interval]}</div>
+        `}
+        >
+          {timeLeft[interval]}
+        </div>
         <div className={styles.timerText}>{interval}</div>
       </div>
     );
@@ -73,29 +86,199 @@ export default function Home() {
 
             {/* Text Block  */}
 
-            <div className={`col-12 col-md-7 p-md-5 order-md-first text-center text-md-start`}>
+            <div
+              className={`col-12 col-md-7 p-md-5 order-md-first text-center text-md-start`}
+            >
               <div className={`${styles.launchingHeading} my-3`}>
-                LAUNCHING&nbsp;SOON....
+                Connect with Astrologers!
               </div>
 
-              <p className={`mb-1 text-center text-md-start ${styles.mainText} `}>
-                Dreshkan is an all-in-one solution for astrology and horoscopic needs. Get ready to join us on our journey in  world of astrology.
-                We will be providing plethora of services from live consultation to match making reports. 
-                Do not worry about your future as we are here to accompany you...
-                
+              <p
+                className={`mb-1 text-center text-md-start ${styles.mainText} `}
+              >
+                Dreshkan is an all-in-one solution for astrology and horoscopic
+                needs. Get ready to join us on our journey in world of
+                astrology. We will be providing plethora of services from live
+                consultation to match making reports. Do not worry about your
+                future as we are here to accompany you...
               </p>
 
               {/* Timer  */}
               <div className="text-white d-flex justify-content-center justify-content-md-start gap-3 my-3">
-                {timerComponents.length ? (
-                  timerComponents
-                ) : (
-                  ""
-                )}
+                {timerComponents.length ? timerComponents : ""}
               </div>
             </div>
           </div>
         </div>
+
+        {/* Services Container */}
+
+        <div className={`${styles.services_container} `}>
+          {/* Title Text  */}
+          <div className={styles.containerTitle}>Our Services</div>
+
+          {/* Cards */}
+          <div className={styles.cards_container}>
+            <div className={`${styles.card} ${styles.chat_card}`}>
+              {/* Icon  */}
+              <div className={styles.icon}>
+                <BsChatSquareDots />
+              </div>
+
+              {/* Title  */}
+              <div className={styles.title}>Chat With Astrologers</div>
+
+              {/* Text  */}
+              <div className={styles.text}>
+                Chat with re-knowned astrologers at minimial fees.
+              </div>
+            </div>
+
+            <div className={`${styles.card} ${styles.call_card}`}>
+              {/* Icon  */}
+              <div className={styles.icon}>
+                <FiPhone />
+              </div>
+
+              {/* Title  */}
+              <div className={styles.title}>Talk to Astrologers</div>
+
+              {/* Text  */}
+              <div className={styles.text}>
+                Connect with astrologers on video and voice call{" "}
+              </div>
+            </div>
+
+            <div className={`${styles.card} ${styles.read_card}`}>
+              {/* Icon  */}
+              <div className={styles.icon}>
+                <HiOutlineDocumentText />
+              </div>
+
+              {/* Title  */}
+              <div className={styles.title}>Read Astrology Blogs</div>
+
+              {/* Text  */}
+              <div className={styles.text}>
+                Stay updated to what's happening in astrology world.
+              </div>
+            </div>
+
+            <div className={`${styles.card} ${styles.shop_card}`}>
+              {/* Icon  */}
+              <div className={styles.icon}>
+                <FiShoppingCart />
+              </div>
+
+              {/* Title  */}
+              <div className={styles.title}>Shop on Dreshkan Shop</div>
+
+              {/* Text  */}
+              <div className={styles.text}>
+                Buy any astrological item and book poojas using shop.
+              </div>
+            </div>
+
+            <div className={`${styles.card} ${styles.report_card}`}>
+              {/* Icon  */}
+              <div className={styles.icon}>
+                <BsBarChart />
+              </div>
+
+              {/* Title  */}
+              <div className={styles.title}>Get Detailed Reports</div>
+
+              {/* Text  */}
+              <div className={styles.text}>
+                Detailed Reports on basis of your Kundali.
+              </div>
+            </div>
+
+            <div className={`${styles.card} ${styles.live_card}`}>
+              {/* Icon  */}
+              <div className={styles.icon}>
+                <MdOndemandVideo />
+              </div>
+
+              {/* Title  */}
+              <div className={styles.title}>Live Broadcast</div>
+
+              {/* Text  */}
+              <div className={styles.text}>
+                Connect on Live Broadcast and ask your concerns.
+              </div>
+            </div>
+
+            <div className={`${styles.card} ${styles.daily_card}`}>
+              {/* Icon  */}
+              <div className={styles.icon}>
+                <BiPlanet />
+              </div>
+
+              {/* Title  */}
+              <div className={styles.title}>Daily Horoscope</div>
+
+              {/* Text  */}
+              <div className={styles.text}>
+                Read about how will your day go based on your zodiac sign.{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* About Container */}
+        <div className={styles.about_container}>
+          {/* container title */}
+          <div className={styles.container_title}>About Dreshkan</div>
+
+          {/* Logo Text Container   */}
+          <div className={`container-fluid  `}>
+            <div className={`row justify-content-center justify-content-md-start align-items-center  `}>
+              {/* Logo  */}
+              <div className={`col-9  col-md-3 ${styles.image_container}`}>
+                
+              <Image src={Logo} />
+
+              </div>
+
+              {/* Text  */}
+              <div className={`col-12 col-md-7  order-md-first text-center text-md-start ${styles.text_container}`}>
+               <p>
+                Dreshkan is one of the most authentic astrology destinations for
+                not only those who are seeking astrological assistance, but also
+                for high-level astrological research and development on wide
+                scale. It is a prolific astrological source for people to help
+                them out from mundane questions to specialized queries. Our aim
+                is to ameliorate those who are facing problems and betterment of
+                humanity using divine science of astrology. 
+                </p>
+                <p>
+                Keeping this all-embracing vision in mind, famous astrologer Acharya Arti
+                Sharma and Mrs Anupama Raizada, embarked Dreshkan in year 2022,
+                in guidence of renowned astrologer Shailendra Pandey, to put
+                astrological wisdom for help of ailing mankind. The Dreshkan
+                team that works in his guidance cones of many expert astrologers
+                pertaining to different schools of astrology.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+{/* FAQ Container */}
+<div className={styles.faq_container} >
+
+  <div className={styles.container_title} >Frequently Asked Questions</div> 
+
+  <div className=" ">
+
+    <Faqs/>
+
+  </div>
+
+</div>
+
+
       </main>
     </>
   );
