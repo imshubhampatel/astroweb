94ad8482ed7b2c1668dca9d5dd145c8c335d759d
arpit <arpitmehta.cse18@iitbhu.ac.in>
2022-03-28 13:22:38 +0530

    error

--- components/Layout.js
+++ components/Layout.js
@@ -5,10 +5,6 @@ import Footer from './Footer'
 const Layout = ({children}) => {
         return (
           <>
-          {/* <!-- Google Tag Manager (noscript) --> */}
-          {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TG28SWS"
-          height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript> */}
-          {/* <!-- End Google Tag Manager (noscript) --> */}
           <div className="vh-100 d-flex flex-column" >
             <Navbar />
 
