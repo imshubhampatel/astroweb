e9d4b9cfc3e4b671418550b328a2940222674fab
arpit <arpitmehta.cse18@iitbhu.ac.in>
2022-04-02 14:42:16 +0530

    time

--- components/adminPanel/meetingCard.js
+++ components/adminPanel/meetingCard.js
@@ -53,7 +53,7 @@ export default function MeetingCard({ data, type }) {
         </div>
         <div className={`${styles.orderDetailText}`}>
           Order id: {data?.id} <br />
-          {data?.scheduledTime ? new Date(data?.scheduledTime.seconds).toLocaleTimeString("en-US"): null}
+          {data?.scheduledTime.toDate().toDateString() + " "}{data?.scheduledTime ? new Date(data?.scheduledTime.seconds).toLocaleTimeString([], {timeStyle: 'short'}): null}
         </div>
 
         <div className={`${styles.orderStatus}  ${styles.orderStatusSuccess} `}>
